835f0b01393262634480aa85fd004731a35abfc5
heyitslauuu <laurencecadag1@gmail.com>
2023-10-15 18:56:28 +0800

    Tailwind Theme Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#280031",
+        "secondary": "#3c1f42",
+        "accent": "#6242fc",
+        "support": "#36213d",
+        "link": "#e5ef70"
+      }
+    },
   },
   plugins: [],
 }
