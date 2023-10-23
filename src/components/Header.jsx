5de8d438959fb05437fcfc63d1c11ec6451e285c
heyitslauuu <laurencecadag1@gmail.com>
2023-10-23 17:21:19 +0800

    Header Component Redirect to root

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,11 @@ function Header() {
       <div className="flex items-center  justify-between">
         <div className="flex items-center gap-2">
           <FontAwesomeIcon icon={faBars} className="text-xl cursor-pointer"/>
-          <h1 className="font-bold text-xl">Game Shop</h1>
+          <Link to="/"><h1 className="font-bold text-xl">Game Shop</h1></Link>
         </div>
         <div className="flex items-center gap-4">
           <FontAwesomeIcon icon={faMagnifyingGlass} className="text-xl"/>
-          <Link to="signup" className="bg-accent cursor-pointer rounded-2xl px-4 py-1 font-medium">Sign Up</Link>
+          <Link to="/signup" className="bg-accent cursor-pointer rounded-2xl px-4 py-1 font-medium">Sign Up</Link>
         </div>
       </div>
     </div>
