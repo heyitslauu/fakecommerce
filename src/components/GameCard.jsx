3b5dd39ed32ed737d0c9d334df51ca0481dbb904
heyitslauuu <laurencecadag1@gmail.com>
2023-10-23 17:21:34 +0800

    GameCard component onclick handler

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types"
 
-function GameCard ({name, alt, src}) {
+function GameCard ({name, alt, src, handleClick}) {
   return (
-    <div className="text-white bg-secondary rounded-lg">
+    <div className="text-white bg-secondary rounded-lg" onClick={handleClick}>
       <img src={src} alt={alt} className="w-full rounded-t-lg"/>
       <div className="px-4 py-2">
         <h1 className="text-center font-medium text-xl md:text-lg">{name}</h1>
@@ -14,7 +14,8 @@ function GameCard ({name, alt, src}) {
 GameCard.propTypes = {
   name: PropTypes.string,
   alt: PropTypes.string,
-  src: PropTypes.string
+  src: PropTypes.string,
+  handleClick: PropTypes.func
 }
 
 
